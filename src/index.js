5c147e8b95f17ee4c39e530bcb20e0442f4952c0
royweb <xytank@gmail.com>
2022-02-22 14:01:51 -0500

    Change cube to ground

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const ratio = window.innerWidth / window.innerHeight;
 // Creating the scene
 const scene = new THREE.Scene();
 
-/*  1) Field of view
+/* 
+    1) Field of view
     2) Ratio
     3) Clipping start, where the camera is going to start rendering stuff from
     4) Clipping end
@@ -14,12 +15,18 @@ const renderer = new THREE.WebGLRenderer();     // What the user should see
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );       // Add it to the DOM
 
-// Instantiated geometry element
-const geometry = new THREE.BoxGeometry();
+/*
+    Instantiated geometry element
+    1) Width
+    2) Height
+    3) Depth
+*/
+const geometry = new THREE.BoxGeometry(50, 0.1, 50);
 
 const light = new THREE.AmbientLight(0x404040);
 
-/*  Direction of the light
+/* 
+    Direction of the light
     1) Color
     2) Brightness Value
 */ 
@@ -30,27 +37,27 @@ light.add(dLight);
 scene.add(light);
 
 // Instantiated material element
-const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
 
 // Create a mesh
-const cube = new THREE.Mesh( geometry, material );
+const ground = new THREE.Mesh( geometry, material );
 
-// Add cube to scene
-scene.add( cube );
+// Add ground to scene
+scene.add( ground );
 
-// Move the camera to see the cube
-camera.position.z = 6;
+/*
+    Move the camera to see the ground
+    1) X
+    2) Y
+    3) Z
+*/
+camera.position.set(5, 15, 15);
 
 // For animation
 // Every seconds this function is called
 function animate() {
-    // Rotate the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    // Infinite loop
-	requestAnimationFrame( animate );
-
+	requestAnimationFrame( animate );   // Infinite loop
+    camera.lookAt(ground.position);     // Set the camera to look at ground
 	renderer.render( scene, camera );
 }
 
